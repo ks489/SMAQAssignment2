b87d29f854e86696e4c80b261f75d4d65d2a9188
Kyle Spindler <AppleZa-Laptop@Kyles-MacBook.local>
2017-11-26 11:40:17 +0000

    Added some unit tests

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/bb/a0a34be79ad20017179495dd1b515da4
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test1() {
+		fail("Not yet implemented");
+	}
+
+}
--- BlackboxTesting/src/TestTestCase.java
+++ BlackboxTesting/src/TestTestCase.java
@@ -1,12 +1,19 @@
 import static org.junit.Assert.*;
 
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
 import org.junit.Test;
 
 public class TestTestCase {
 
 	@Test
 	public void test() {
+		CharSequence cs = "Tue, 3 Jun 2008 11:05:30 GMT";
+
+		TemporalAccessor ta = DateTimeFormatter.RFC_1123_DATE_TIME.parse(cs);
 		fail("Not yet implemented");
+		//Throws DateTimeParseExeption
 	}
 	
 	@Test
